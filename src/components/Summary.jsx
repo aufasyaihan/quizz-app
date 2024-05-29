28281c7f3d40a41ffbe619c30fdc65a09276c017
aufa azzahidi <aufa.azzahidi@gmail.com>
2024-05-29 10:33:34 +0700

    added sumamry percentage question share

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -2,21 +2,27 @@ import completeImg from "../assets/quiz-complete.png";
 import QUESTIONS from "../questions.js";
 
 export default function Summary({ userAnswers }) {
+    const skippedAnswer = userAnswers.filter((answer) => answer === null);
+    const correctAnswer = userAnswers.filter((answer, index) => answer === QUESTIONS[index].answers[0]);
+
+    const skippedAnswerPercentage = Math.round(skippedAnswer.length / userAnswers.length * 100);
+    const correctAnswerPercentage = Math.round(correctAnswer.length / userAnswers.length * 100);
+    const incorrectAnswerPercentage = 100 - skippedAnswerPercentage - correctAnswerPercentage;
   return (
     <section id="summary">
       <img src={completeImg} alt="finished quiz" />
       <h2>Quiz Completed</h2>
       <div id="summary-stats">
         <p>
-          <span className="number">10%</span>
+          <span className="number">{skippedAnswerPercentage}%</span>
           <span className="text">skipped</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{correctAnswerPercentage}%</span>
           <span className="text">answered correctly</span>
         </p>
         <p>
-          <span className="number">10%</span>
+          <span className="number">{incorrectAnswerPercentage}%</span>
           <span className="text">answered incorrectly</span>
         </p>
       </div>
